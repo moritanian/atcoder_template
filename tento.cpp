3dcba53a39a673b98a653e197dfab5685fa65f50
moritanian <pickit2express@gmail.com>
2022-07-24 01:47:56 +0900

    fix tento

--- tento.cpp
+++ tento.cpp
@@ -1,11 +1,21 @@
 
-template <typename T>
-int tento(vector<T>& V) {
+ll tento(const vector<ll> &L) {
   atcoder::fenwick_tree<ll> ft(L.size());
-  int ans = 0;
-  for (int i = 0; i < L.size(); i++) {
+  ll ans = 0;
+  REP(i, L.size()) {
     ans += ft.sum(L[i], L.size());
     ft.add(L[i], 1);
   }
+  return ans;
+}
+
+vector<ll> compress(const vector<ll> &X) {
+  vector<pair<ll, ll>> ps;
+  REP(i, X.size()) { ps.emplace_back(X[i], i); }
+  sort(ALL(ps));
+
+  vector<ll> ans(X.size());
+  REP(i, X.size()) { ans[ps[i].second] = i; }
+
   return ans;
 }
